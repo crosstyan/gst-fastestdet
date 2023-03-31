dd80cb97b6c7678d1c9d42b6bebeffb72b5dc385
root <root@cross-laptop>
2023-03-31 14:41:33 +0800

    example format

--- example/include/yolo-fastestv2.h
+++ example/include/yolo-fastestv2.h
@@ -3,56 +3,55 @@
 
 #include "net.h"
 
-#include <vector>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
-class TargetBox
-{
+class TargetBox {
 private:
-    float getWidth() { return (x2 - x1); };
-    float getHeight() { return (y2 - y1); };
+  float getWidth() { return (x2 - x1); };
+  float getHeight() { return (y2 - y1); };
 
 public:
-    int x1;
-    int y1;
-    int x2;
-    int y2;
+  int x1;
+  int y1;
+  int x2;
+  int y2;
 
-    int cate;
-    float score;
+  int cate;
+  float score;
 
-    float area() { return getWidth() * getHeight(); };
+  float area() { return getWidth() * getHeight(); };
 };
 
-class yoloFastestv2
-{
+class yoloFastestv2 {
 private:
-    ncnn::Net net;
-    std::vector<float> anchor;
+  ncnn::Net net;
+  std::vector<float> anchor;
 
-    char *inputName;
-    char *outputName1;
-    char *outputName2;
+  char *inputName;
+  char *outputName1;
+  char *outputName2;
 
-    int numAnchor;
-    int numOutput;
-    int numThreads;
-    int numCategory;
-    int inputWidth, inputHeight;
+  int numAnchor;
+  int numOutput;
+  int numThreads;
+  int numCategory;
+  int inputWidth, inputHeight;
 
-    float nmsThresh;
+  float nmsThresh;
 
-    int nmsHandle(std::vector<TargetBox> &tmpBoxes, std::vector<TargetBox> &dstBoxes);
-    int getCategory(const float *values, int index, int &category, float &score);
-    int predHandle(const ncnn::Mat *out, std::vector<TargetBox> &dstBoxes, 
-                   const float scaleW, const float scaleH, const float thresh);
+  int nmsHandle(std::vector<TargetBox> &tmpBoxes,
+                std::vector<TargetBox> &dstBoxes);
+  int getCategory(const float *values, int index, int &category, float &score);
+  int predHandle(const ncnn::Mat *out, std::vector<TargetBox> &dstBoxes,
+                 const float scaleW, const float scaleH, const float thresh);
 
 public:
-    yoloFastestv2();
-    ~yoloFastestv2();
+  yoloFastestv2();
+  ~yoloFastestv2();
 
-    int loadModel(const char* paramPath, const char* binPath);
-    ncnn::Mat detection(const cv::Mat srcImg, std::vector<TargetBox> &dstBoxes, 
-                  const float thresh = 0.3);
+  int loadModel(const char *paramPath, const char *binPath);
+  ncnn::Mat detection(const cv::Mat srcImg, std::vector<TargetBox> &dstBoxes,
+                      const float thresh = 0.3);
 };
 #endif
